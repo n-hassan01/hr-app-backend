f038326448022a3def40b2b7a9e66352f319acdb
n-hassan01 <naimul.hassan.se@gmail.com>
2024-11-25 17:43:56 +0600

    bismillahir rahmanir rahim

---
+++ hr-app/src/main/java/com/remark_herlan/hr_app/HrAppApplication.java
@@ -0,0 +1,13 @@
+package com.remark_herlan.hr_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAppApplication.class, args);
+	}
+
+}
