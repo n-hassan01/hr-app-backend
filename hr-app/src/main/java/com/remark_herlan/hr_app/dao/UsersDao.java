46e87eb7c9c700545bd15fca5b04f2a69139e8b5
n-hassan01 <naimul.hassan.se@gmail.com>
2024-11-26 15:52:40 +0600

    RHR-13: create API for users table using spring and postgresql

---
+++ hr-app/src/main/java/com/remark_herlan/hr_app/dao/UsersDao.java
@@ -0,0 +1,21 @@
+package com.remark_herlan.hr_app.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.remark_herlan.hr_app.model.Users;
+
+/**
+ * author: Naimul Hassan
+ * 
+ * date: 11/26/2024
+ */
+
+@Repository
+public interface UsersDao extends JpaRepository<Users, Integer> {
+
+	Optional<Users> findByUsername(String username);
+
+}
